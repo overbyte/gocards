21d4533b8382cafae6f4ccf866e13fe07c63e6bf
Allandt Bik-Elliott <allandt@gmail.com>
2020-06-08 16:05:46 +0100

    use number of nanoseconds since epoch as a seed for a new random instance to produce actually random positions for the shuffle method

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -78,8 +79,12 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	s := time.Now().UnixNano()
+	source := rand.NewSource(s)
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
